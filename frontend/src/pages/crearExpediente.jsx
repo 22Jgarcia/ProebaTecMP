8b9ff8596a9fc95637a1ba414e4a45bc84c13d56
22Jgarcia <jgarciavn22@gmail.com>
2025-11-22 00:49:51 -0600

    fix: corregir errores tipográficos y mejorar la estructura del formulario en crearExpediente

--- frontend/src/pages/crearExpediente.jsx
+++ frontend/src/pages/crearExpediente.jsx
@@ -1,48 +1,48 @@
 import { useState } from "react";
-import { useNavigate } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function CrearExpediente(){
   const [numero, setNumero]  = useState("");
   const [descripcion, setDescripcion] = useState("");
   const navigate = useNavigate();
 
-  const hadleSumit = async(e)=>{
+  const handleSubmit = async(e)=>{
     e.preventDefault();
-
-    await fetch("http://localhos:3000/expedientes", { 
-      method: "POST",
-      headers: {"content-Type": "application/json"},
-      body: JSON.stringify({numero, descripcion})
-    });
+    //arreglar para enviar
+    await fetch("http://localhost:3000/expedientes", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ numero, descripcion })
+  });
     navigate("/expedientes");
   };
 
   return(
-    <div className="p-6 max-w-xl mx-aut">
-      <h2 className="text-2xl font-semibold mb-6">Crear Expediente</h2>
+     <div className="p-6 max-w-xl mx-auto">
+    <h2 className="text-2xl font-semibold mb-6">Crear Expediente</h2>
 
-      <form onSubmit={hadleSumit}>
-        <input
-          className="p-3 border rounded" 
-          placeholder="Numero" 
-          value={numero}
-          onChange={(e)=> setNumero(e.target.value)}
-          required
-        />
-        
-        <textarea 
-          className="p-3 border rounded"
-          placeholder="Descripcion"
-          value={descripcion}
-          onChange={(e)=> setDescripcion(e.target.value)}
-          required
-        />
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
 
-        
-        <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-          Guardar
-        </button>
-      </form>
-    </div>
+      <input
+        className="p-3 border rounded w-full"
+        placeholder="Número"
+        value={numero}
+        onChange={(e) => setNumero(e.target.value)}
+        required
+      />
+
+      <textarea
+        className="p-3 border rounded w-full"
+        placeholder="Descripción"
+        value={descripcion}
+        onChange={(e) => setDescripcion(e.target.value)}
+        required
+      />
+
+      <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+        Guardar
+      </button>
+    </form>
+  </div>
   );
 }
