7a9f55d0d9d35f1f7961f4d5f02cb4d08fc3294f
22Jgarcia <jgarciavn22@gmail.com>
2025-11-22 00:49:07 -0600

    fix: corregir métodos HTTP en las rutas de expedientes

--- backendMP/routes/expedientes.routes.js
+++ backendMP/routes/expedientes.routes.js
@@ -1,4 +1,4 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const controller = require('../controllers/expedientes.controller');
 
 const router = Router();
@@ -6,7 +6,10 @@ const router = Router();
 router.get("/", controller.listarExpedientes);
 router.post("/", controller.crearExpediente);
 router.get("/:id", controller.obtenerExpediente);
-router.post("/:id/aprobar", controller.aprobarExpediente);
-router.post("/:id/rechazar", controller.rechazoExpediente);
+
+router.get("/:id/indicios", controller.listarIndiciosPorExpediente);
+
+router.put("/:id/aprobar", controller.aprobarExpediente);
+router.put("/:id/rechazar", controller.rechazarExpediente);
 
 module.exports = router;
