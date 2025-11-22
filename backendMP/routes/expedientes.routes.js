59e960ec47433ad56856e237f8c379bcad543e9f
22Jgarcia <jgarciavn22@gmail.com>
2025-11-22 12:55:52 -0600

    feat: agregar requerimiento de rol para crear y modificar expedientes

--- backendMP/routes/expedientes.routes.js
+++ backendMP/routes/expedientes.routes.js
@@ -1,15 +1,16 @@
 const { Router } = require('express');
 const controller = require('../controllers/expedientes.controller');
+const  requireRole = require("../middleware/roles");
 
 const router = Router();
 
 router.get("/", controller.listarExpedientes);
-router.post("/", controller.crearExpediente);
+router.post("/", requireRole(["coordinador"]), controller.crearExpediente);
 router.get("/:id", controller.obtenerExpediente);
 
 router.get("/:id/indicios", controller.listarIndiciosPorExpediente);
 
-router.put("/:id/aprobar", controller.aprobarExpediente);
-router.put("/:id/rechazar", controller.rechazarExpediente);
+router.put("/:id/aprobar", requireRole(["coordinador"]), controller.aprobarExpediente);
+router.put("/:id/rechazar", requireRole(["coordinador"]), controller.rechazarExpediente);
 
 module.exports = router;
