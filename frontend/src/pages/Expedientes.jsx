20a971c4c93311d76db19319a90a3e294efc16da
22Jgarcia <jgarciavn22@gmail.com>
2025-11-22 00:50:02 -0600

    fix: corregir la importación de Link en Expedientes.jsx

--- frontend/src/pages/Expedientes.jsx
+++ frontend/src/pages/Expedientes.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { Link } from "react-router-dom";
 
 export default function Expedientes(){
   const [expedientes, setExpedientes]= useState([]);
