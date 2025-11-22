bb2887e2d9130dee6a90a8f2f90ab3cc4744629c
22Jgarcia <jgarciavn22@gmail.com>
2025-11-21 21:21:05 -0600

    feat: reestructurar App.jsx para incluir autenticación y rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
+import { AuthProvider } from "./context/AuthContext";
+import CrearExpediente from './pages/crearExpediente'
+import Expedientes from './pages/Expedientes'
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
 
-function App() {
+
+export default function App() {
 
   return (
-    <>
-      <h1 className='text-3xl font-bold underline text-green-500'>
-        Hola mundo con tailwind
-      </h1>   
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar>
+          <Routes>
+            <Route path="/" element={<Login />}/>
+            <Route path="/expedientes" element={<Expedientes/>}/>
+            <Route path="/expedientes/nuevo" element={<CrearExpediente />}/>
+            {/* <Route path="/expedientes/:id" element={<DetalleExpediente/>}/>
+            <Route path="/report" element={<Report/>}/> */}
+          </Routes>
+        </Navbar>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
-export default App
+
