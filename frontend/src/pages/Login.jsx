1159dbdf41dc4c300b6cc28db4b24f783ac43d09
22Jgarcia <jgarciavn22@gmail.com>
2025-11-21 20:05:12 -0600

    feat: agregar componente Login para autenticación de usuarios

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function Login(){
+  const {login} = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogin = (rol) =>{
+    login(rol);
+    navigate("/expedientes");
+  }
+
+  return(
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold mb-10">Sistema de Expedientes MP</h1>
+      <button 
+        onClick={()=>handleLogin("tecnico")}
+        className="bg-blue-500 text-white px-6 py-3 rounded-md w-64 mb-5 hober:bg-blue-600"
+      >
+       Ingresar como Tecnico 
+      </button>
+
+      <button
+        onClick={()=>handleLogin("tecnico")}
+        className="bg-green-600 text-white px-6 py-3 rounded-md w-64 hover:bg-green-700"
+      >
+        Ingresar como Coordinador
+      </button>
+    </div>
+  );
+}
+
