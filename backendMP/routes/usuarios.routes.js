077cc26db87f229ea148c5f899c01c39d396ce62
22Jgarcia <jgarciavn22@gmail.com>
2025-11-22 00:49:23 -0600

    feat: agregar rutas para la gestión de usuarios

---
+++ backendMP/routes/usuarios.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const controller = require("../controllers/usuarios.controller");
+
+const router = Router();
+
+router.get("/", controller.listarUsuarios);
+router.post("/login", controller.login);
+router.get("/roles", controller.listarRoles);
+
+module.exports = router;
