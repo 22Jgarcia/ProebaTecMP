c81c7de7ae4e77e0e1dd6e9aeaee3bd8da72823c
22Jgarcia <jgarciavn22@gmail.com>
2025-11-22 00:50:07 -0600

    fix: corregir la importación de useNavigate en Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { useNavigate } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login(){
   const {login} = useContext(AuthContext);
